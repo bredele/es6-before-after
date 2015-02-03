91c065ae0f22c310e8afc9fcf189db5fd8413446
bredele <olivier.wietrich@gmail.com>
2015-02-02 18:54:31 -0700

    update promise

--- examples/promises.js
+++ examples/promises.js
@@ -2,17 +2,16 @@
 function login(name, password) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-        if(name === 'admin' && passord === 'adming') resolve();
+        if(name === 'admin' && password === 'admin') resolve();
         else reject();
     }, 2000);
   });
 }
 
 
-login('admin', 'adming')
+login('admin', 'admin')
   .then(function() {
     console.log('success')
-  })
-  .catch(function(e) {
-    console.log('error');
+  }, function() {
+      console.log('error');
   });
