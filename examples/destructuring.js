a7621898aaff7036923679cfd2ea4139ecea501a
bredele <olivier.wietrich@gmail.com>
2015-02-02 18:28:06 -0700

    update example destructuring

--- examples/destructuring.js
+++ examples/destructuring.js
@@ -12,3 +12,16 @@ console.log(width, height);
 
 var { width: w, height: h } = size;
 console.log(w, h);
+
+function stuff(name, { country, city = 'calgary'}) {
+  console.log(name);
+  console.log(country);
+  console.log(city);
+}
+
+stuff('olivier', {
+	country: 'canada'
+});
+
+var obj = {latitude, longitude};
+console.log(obj.latitude, obj.longitude);
