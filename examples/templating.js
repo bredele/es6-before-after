4b1235ed191c1a567d3678537e18139ee833fae8
bredele <olivier.wietrich@gmail.com>
2015-02-02 18:45:34 -0700

    update example templating

--- examples/templating.js
+++ examples/templating.js
@@ -1,18 +1,9 @@
-var arch_nemesis = "Newman";
-console.log(`Jerry: Hello, ${arch_nemesis}`);
+var github = 'bredele';
+console.log(`my github is ${github}`);
 
-canvas.addEventListener('click', e => {
-    console.log(`click @ (${e.pageX}, ${e.pageY})`);
-});
 
-function length(x, y) {
-    return Math.sqrt(x * x + y * y);
+function add(x, y) {
+    return x + y;
 }
 
-console.log(`3^2 + 4^2 = ${length(3,4)}^2`);
-
-var html = `<p>
-    <b>Jerry:</b> Hello, ${arch_nemesis}
-</p>`;
-
-console.log(html);
+console.log(`1 + 2 = ${add(1,2)}`);
