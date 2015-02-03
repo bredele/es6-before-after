8f73bcb539979be6b29067969d4dc7ca075079de
bredele <olivier.wietrich@gmail.com>
2015-02-02 18:38:27 -0700

    update example promise

--- examples/promises.js
+++ examples/promises.js
@@ -1,22 +1,18 @@
-function timeout(duration = 0) {
-    if (duration > 1000) throw `${duration}ms is too long`;
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(duration);
-        }, duration);
-    });
+
+function login(name, password) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        if(name === 'admin' && passord === 'adming') resolve();
+        else reject();
+    }, 2000);
+  });
 }
 
-var p = timeout(500).then(() => {
-    console.log("do some stuff");
-    return timeout(500);
-}).then(duration => {
-    console.log(`some stuff took ${duration}ms`);
-    console.log("do some stuff in parallel");
-    return Promise.all([timeout(100), timeout(200)]);
-}).then(times => {
-    console.log(`parallel stuff took ${times.map(t=>`${t}ms`)}`);
-    return timeout(2000);   // should throw
-}).catch(err => {
-    console.log(err);
-});
+
+login('admin', 'adming')
+  .then(function() {
+    console.log('success')
+  })
+  .catch(function(e) {
+    console.log('error');
+  });
